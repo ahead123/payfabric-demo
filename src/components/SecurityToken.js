4ed987a4f3bc97e133ec3d1e370cd06379fddf57
Arthur Head <arthurhead@Arthurs-MBP.lan>
2024-06-05 08:30:47 -0700

    added transacations

--- src/components/SecurityToken.js
+++ src/components/SecurityToken.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Row, Col, Button, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import postman_token_create from '../images/postman_token_create.png';
 
 const SecurityToken = () => {
@@ -25,60 +26,81 @@ const SecurityToken = () => {
       });
   }, []);
   return (
-    <Row>
-      <Col className="text-center">
-        <h3>PayFabric API Authentication</h3>
-        <p>
-          To make requests to the PayFabric API, you need to
-          authenticate first. The required credentials{' '}
-          <code>DCN</code>, <code>Device ID</code>, and{' '}
-          <code>Device Password</code> are provided by PayFabric via
-          the web portal on the Device Management page. After
-          successfully authenticating with the PayFabric API, you will
-          receive a security token that you can use to make requests
-          to the API.
-        </p>
-        <p>
-          For testing purposes, you can use the sandbox API. The
-          sandbox API is a test environment that simulates the
-          PayFabric production environment. You can use the sandbox
-          API to test your integration without processing live
-          transactions.
-        </p>
-        <p>
-          <strong>Sandbox Endpoint: </strong>
-          <code>
-            https://sandbox.payfabric.com/payment/api/token/create
-          </code>
-          <br />
-          <strong>Device ID: </strong>
-          <code>2:86c76aa9-b700-460c-a69f-c0b194f0279b</code>
-          <br />
-          <strong>Device Password: </strong>
-          <code>TSEvaluation@001</code>
-          <br />
-        </p>
-        <p>
-          <Image
-            alt="postman token create"
-            src={postman_token_create}
-            thumbnail
-          />
-        </p>
-      </Col>
-      <Col className="text-center">
-        <h3>Token</h3>
-        <p>
-          <code>{token}</code>
-        </p>
-        <Button
-          onClick={() => {
-            navigator.clipboard.writeText(token);
-          }}>
-          Copy
-        </Button>
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col className="text-center">
+          <h3>PayFabric API Authentication</h3>
+          <p>
+            To make requests to the PayFabric API, you need to
+            authenticate first.
+          </p>
+          <p>
+            The required credentials <code>Device ID</code>, and{' '}
+            <code>Device Password</code> are provided by PayFabric via
+            the web portal on the Device Management page.
+          </p>
+          <p>
+            After successfully authenticating with the PayFabric API,
+            you will receive a security token that you can use to make
+            requests to the API.
+          </p>
+          <p>
+            For testing purposes, you can use the sandbox API. The
+            sandbox API is a test environment that simulates the
+            PayFabric production environment. You can use the sandbox
+            API to test your integration without processing live
+            transactions.
+          </p>
+          <p>
+            <strong>Sandbox Endpoint: </strong>
+            <code>
+              https://sandbox.payfabric.com/payment/api/token/create
+            </code>
+            <br />
+            <strong>Device ID: </strong>
+            <code>2:86c76aa9-b700-460c-a69f-c0b194f0279b</code>
+            <br />
+            <strong>Device Password: </strong>
+            <code>TSEvaluation@001</code>
+            <br />
+            <strong>API Documentation Reference: </strong>
+            <code>
+              https://github.com/PayFabric/Hosted-Pages/blob/master/Sections/Security%20Token.md
+            </code>
+            <br />
+          </p>
+          <p>
+            <Image
+              alt="postman token create"
+              src={postman_token_create}
+              thumbnail
+            />
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center">
+          <h3>Token</h3>
+          <p>
+            <code>{token}</code>
+          </p>
+          <Button
+            onClick={() => {
+              navigator.clipboard.writeText(token);
+            }}>
+            Copy
+          </Button>
+        </Col>
+      </Row>
+      <Row className="mt-5 mb-5">
+        <Col className="text-center">
+          <Link to="/">Back</Link>
+        </Col>
+        <Col className="text-center">
+          <Link to="/transaction/create">Next</Link>
+        </Col>
+      </Row>
+    </>
   );
 };
 export default SecurityToken;
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,40 +1,138 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Row, Col, Button, Image } from 'react-bootstrap';
+import postman_transaction_create from '../images/postman_transaction_create.png';
+import github_transaction_payload from '../images/github_transaction_payload.png';
 
 const Transaction = () => {
   const [key, setKey] = useState(null);
   useEffect(() => {
-    fetch('https://sandbox.payfabric.com/payment/api/token/create', {
-      method: 'GET',
+    fetch(
+      'https://sandbox.payfabric.com/payment/api/transaction/create',
+      {
+        method: 'POST',
 
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization:
-          '2:86c76aa9-b700-460c-a69f-c0b194f0279b|TSEvaluation@001',
-      },
-    })
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization:
+            '2:86c76aa9-b700-460c-a69f-c0b194f0279b|TSEvaluation@001',
+        },
+        body: JSON.stringify({
+          Amount: '29.99',
+          BatchNumber: '',
+          Currency: 'USD',
+          Customer: 'JOHNDOE0001',
+          Document: {
+            Head: [],
+            Lines: [],
+            UserDefined: [],
+          },
+          PayDate: '',
+          ReferenceKey: null,
+          ReferenceTrxs: [],
+          ReqAuthCode: '',
+          SetupId: 'EVO US_CC',
+          Shipto: {
+            City: '',
+            Country: '',
+            Customer: '',
+            Email: '',
+            Line1: '',
+            Line2: '',
+            Line3: '',
+            Phone: '',
+            State: '',
+            Zip: '',
+          },
+          TrxUserDefine1: '',
+          TrxUserDefine2: '',
+          TrxUserDefine3: '',
+          TrxUserDefine4: '',
+          Type: 'Sale',
+        }),
+      }
+    )
       .then((response) => {
         console.log(response);
         return response.json();
       })
       .then((data) => {
         console.log(data);
-        setKey(data.key);
+        localStorage.setItem('key', data.Key);
+        setKey(data.Key);
       });
   }, []);
 
   return (
-    <Row>
-      <Col className="text-center">
-        <h3>Create Transaction</h3>
-        <p>
-          A transaction is a unit of work that is performed against a
-          database. It is the actual work that is done, such as
-          inserting data, updating data, or deleting data.
-        </p>
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col className="text-center">
+          <h3>Transactions</h3>
+          <p>
+            The PayFabric API allows you to create and process
+            transactions.
+          </p>
+          <p>
+            After a transaction is successfully created the API will
+            return a <code>key</code> that you can use to reference
+            the transaction and update details of the transaction.
+          </p>
+          <p>
+            The <code>key</code> can also be utilized to embed the
+            PayFabric Hosted Payment page in your application.
+          </p>
+          <p>
+            <strong>Sandbox Endpoint: </strong>
+            <code>
+              https://sandbox.payfabric.com/payment/api/transaction/create
+            </code>
+            <br />
+            <strong>API Documentation Reference: </strong>
+            <code>
+              https://github.com/PayFabric/APIs/blob/master/PayFabric/Sections/Transactions.md#create-a-transaction
+            </code>
+            <br />
+            <strong>Transaction Data JSON format: </strong>
+            <Image
+              alt="github transaction payload"
+              src={github_transaction_payload}
+              thumbnail
+            />
+            <br />
+          </p>
+          <p>
+            <Image
+              alt="postman transaction create"
+              src={postman_transaction_create}
+              thumbnail
+            />
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center">
+          <h3>Key</h3>
+          <p>
+            <code>{key}</code>
+          </p>
+          <Button
+            onClick={() => {
+              navigator.clipboard.writeText(key);
+            }}>
+            Copy
+          </Button>
+        </Col>
+      </Row>
+      <Row className="mt-5 mb-5">
+        <Col className="text-center">
+          <Link to="/token/create">Back</Link>
+        </Col>
+        <Col className="text-center">
+          <Link to="/payment-sheet/create">Next</Link>
+        </Col>
+      </Row>
+    </>
   );
 };
 
